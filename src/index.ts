8f1e7f5e770115468b2be60f6a8d31c74e7ec425
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-06-08 00:20:34 -0300

    Clase 2: DisplayList y Containers

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,74 @@
-import { Application, Loader, Sprite } from "pixi.js";
+import { Application, Container, Loader, Point, Sprite } from "pixi.js";
 
 const app = new Application({
   view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
   resolution: window.devicePixelRatio || 1,
   autoDensity: true,
   backgroundColor: 0x6495ed,
-  width: 640,
-  height: 480,
+  width: 1280,
+  height: 720,
 });
 
-Loader.shared.add({ url: "./dino.png", name: "myDino" });
-Loader.shared.add({ url: "./clampy.png", name: "Clampy" });
+// Cambio de Tamaño
+window.addEventListener("resize", () => {
+  // Escalas del Juego
+  const scaleX = window.innerWidth / app.screen.width;
+  const scaleY = window.innerHeight / app.screen.height;
+  const scale = Math.min(scaleX, scaleY);
+
+  // Tamaño del Juego
+  const gameWidth = Math.round(app.screen.width * scale);
+  const gameHeight = Math.round(app.screen.height * scale);
+
+  // Centrado del Juego
+  const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+  const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+
+  // Tamaño de renderizado del Juego
+  app.view.style.width = gameWidth + "px";
+  app.view.style.height = gameHeight + "px";
+
+  app.view.style.marginLeft = marginHorizontal + "px";
+  app.view.style.marginRight = marginHorizontal + "px";
+
+  app.view.style.marginTop = marginVertical + "px";
+  app.view.style.marginBottom = marginVertical + "px";
+});
+window.dispatchEvent(new Event("resize"));
+
+Loader.shared.add({ url: "./dino.png", name: "Dino" });
+Loader.shared.add({ url: "./dinohat.png", name: "Hat" });
 
 Loader.shared.onComplete.add(() => {
-  const clampy: Sprite = Sprite.from("myDino");
+  //------------Dinosaurio--------------//
+  const dino: Sprite = Sprite.from("Dino");
+  dino.scale.set(0.1, 0.1);
+  dino.position.set(100, 100);
 
-  console.log("Hola mundo!", clampy.width, clampy.height);
+  //---------------Gorro----------------//
+  const hat: Sprite = Sprite.from("Hat");
+  hat.scale.set(0.2, 0.2);
+  hat.position.set(157, 50);
 
-  //clampy.anchor.set(0);
+  //-------------Container-------------//
+  const dinoWithHat: Container = new Container();
 
-  clampy.x = 0;
-  clampy.y = 0;
+  dinoWithHat.addChild(dino);
+  dinoWithHat.addChild(hat);
 
-  app.stage.addChild(clampy);
+  dinoWithHat.scale.set(1.2);
+  dinoWithHat.x = 300;
+  dinoWithHat.y = 100;
+  app.stage.addChild(dinoWithHat);
+
+  console.log(hat.toGlobal(new Point()));
+  console.log(hat.parent.toGlobal(hat.position));
+
+  // const aux = hat.parent.toLocal(new Point(0, 0));
+  // hat.position.x = aux.x;
+  // hat.position.y = aux.y;
+  // app.stage.addChild(dino);
+  // app.stage.addChild(hat);
 });
 
 Loader.shared.load();
